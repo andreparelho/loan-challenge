de9a3454d346f548df77b97e03b515bfc8d47809
Andre Parelho <parelhoneves.andre@gmail.com>
2024-06-27 23:21:06 -0300

    feat: update response

--- app/src/main/java/br/com/bank/app/controller/dto/LoanResponse.java
+++ app/src/main/java/br/com/bank/app/controller/dto/LoanResponse.java
@@ -1,8 +1,9 @@
 package br.com.bank.app.controller.dto;
 
 import br.com.bank.app.model.LoanModel;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
-public record LoanResponse (String customer, List<LoanModel> loans) {
+public record LoanResponse (@JsonProperty("customer") String customer, @JsonProperty("loans") List<LoanModel> loans) {
 }
