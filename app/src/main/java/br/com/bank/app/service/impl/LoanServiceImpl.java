e4bdfa75215cc9a6b2dd22ff89201637548bb4b6
Andre Parelho <parelhoneves.andre@gmail.com>
2024-07-12 22:05:04 -0300

    feat: add exception to name param

--- app/src/main/java/br/com/bank/app/service/impl/LoanServiceImpl.java
+++ app/src/main/java/br/com/bank/app/service/impl/LoanServiceImpl.java
@@ -2,13 +2,16 @@ package br.com.bank.app.service.impl;
 
 import br.com.bank.app.controller.dto.LoanRequest;
 import br.com.bank.app.controller.dto.LoanResponse;
+import br.com.bank.app.exception.request.NameIsBlankRequestException;
 import br.com.bank.app.factory.LoanEngine;
-import br.com.bank.app.factory.LoanFactory;
 import br.com.bank.app.model.LoanModel;
 import br.com.bank.app.service.LoanService;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+
+import static br.com.bank.app.util.LoanConstants.*;
+
 
 @Service
 public class LoanServiceImpl implements LoanService {
@@ -20,6 +23,10 @@ public class LoanServiceImpl implements LoanService {
 
     @Override
     public LoanResponse getLoans(LoanRequest loanRequest){
+        if (loanRequest.customerDTO().getName().isBlank() || loanRequest.customerDTO().getName() == null){
+            throw new NameIsBlankRequestException(NAME_NOT_BLANK_MSG);
+        }
+
         List<LoanModel> loans = this.loanEngine.getLoanList(loanRequest);
         return new LoanResponse(loanRequest.customerDTO().getName(), loans);
     }
